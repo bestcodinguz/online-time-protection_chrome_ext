ce05b043c5dc19104fab4c5a0b54ebe197f9893f
DiamondSystems <me@diamondsystems.org>
2022-10-18 14:19:28 +0500

    test user script

---
+++ script.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name Online time protection
+// @description A script that helps protect precious time online
+// @author DiamondSystems
+// @license MIT
+// @version 0.1
+// @include *
+// @grant none
+// ==/UserScript==
+
+(function (window, undefined) {
+    var w = (typeof unsafeWindow !== undefined) ? unsafeWindow : window;
+
+    if (w.self !== w.top)
+        return;
+
+    // if (! /https:\/\/www\.yourcity\.com/.test(w.location.href))
+    //     return;
+
+    alert("OK");
+     
+})(window);
