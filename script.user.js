a61f3ac2301bb12cbc299f10613db7d1fe68467c
DiamondSystems <me@diamondsystems.org>
2022-10-19 14:51:47 +0500

    icon for the script

--- script.user.js
+++ script.user.js
@@ -3,12 +3,18 @@
 // @description A script that helps protect precious time online
 // @author DiamondSystems
 // @license MIT
-// @version 0.13
-// @include *
-// @grant none
+// @version 0.1.4
+// @include http://*
+// @include https://*
+// @grant   unsafeWindow
+// @icon    https://github.com/bestcodinguz/online-time-protection_chrome_ext/raw/main/icon.png
+// @noframe
+// @noframes
 // ==/UserScript==
 
 (function (window, undefined) {
+    "use strict";
+
     var w = (typeof unsafeWindow !== undefined) ? unsafeWindow : window;
 
     if (w.self !== w.top)
